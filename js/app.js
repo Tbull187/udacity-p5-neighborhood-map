f9355896a6b95399e1099e824216a467d15330b3
Tristan Bull <tristan.tbull@gmail.com>
2016-04-12 20:33:00 -0700

    First commit. Just pulled in Google maps

---
+++ js/app.js
@@ -0,0 +1,48 @@
+function initMap() {
+  var myLatlng = {lat: -25.363, lng: 131.044};
+
+  var map = new google.maps.Map(document.getElementById('map'), {
+    zoom: 4,
+    center: myLatlng
+  });
+
+  var marker = new google.maps.Marker({
+    position: myLatlng,
+    map: map,
+    title: 'Click to zoom'
+  });
+
+  map.addListener('center_changed', function() {
+    // 3 seconds after the center of the map has changed, pan back to the
+    // marker.
+    window.setTimeout(function() {
+      map.panTo(marker.getPosition());
+    }, 3000);
+  });
+
+  marker.addListener('click', function() {
+    map.setZoom(8);
+    map.setCenter(marker.getPosition());
+  });
+}
+
+
+// function initMap() {
+
+// 	// Create a map object and specify the DOM element for display.
+// 	var map = new google.maps.Map(document.getElementById('map'), {
+// 		zoom: 0,
+// 		center: {lat: 47.606, lng: -122.332}
+// 	});
+// }
+
+
+// function initMap() {
+// 	var mapOptions = {
+// 		zoom: 0,
+// 		center: new google.maps.LatLng(47.606, -122.332)
+// 	};
+
+// 	var map = new google.maps.Map(document.getElementById('map'), mapOptions);
+// }
+
