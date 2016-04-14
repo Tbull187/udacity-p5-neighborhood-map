94af709a552c9a359bc74eaba4b302eabf6de257
Tristan Bull <tristan.tbull@gmail.com>
2016-04-13 23:00:54 -0700

    Created data Model. Added unique data for location markers.

--- js/app.js
+++ js/app.js
@@ -1,26 +1,53 @@
+
+// This is unique data that will be injected into google map markers
+
+var model = [
+  {
+    title: 'Kerry Park',
+    lat: 47.629552,
+    lng: -122.360127
+  },
+  {
+    title: 'Uncle Ike\'s',
+    lat: 47.613352,
+    lng: -122.302226
+  },
+    {
+    title: 'Showbox Sodo',
+    lat: 47.587926,
+    lng: -122.333627
+  },
+    {
+    title: 'Toulouse Petit',
+    lat: 47.624761,
+    lng: -122.357008
+  },
+    {
+    title: 'GameWorks',
+    lat: 47.611909,
+    lng: -122.334027
+  }
+]
+
+var marker;
+
 function initMap() {
 
-	// Create a map object and specify the DOM element for display.
 	var map = new google.maps.Map(document.getElementById('map'), {
-		zoom: 14,
+		zoom: 13,
 		center: {lat: 47.6062, lng: -122.332}
 	});
 
-  var marker1 = new google.maps.Marker({
+  marker = new google.maps.Marker({
     map: map,
     position: {lat: 47.629552, lng: -122.360127},
     title: 'Kerry Park',
     animation: google.maps.Animation.DROP
   });
 
-  var marker2 = new google.maps.Marker({
-
-  })
-
-  marker1.addListener('click', toggleBounce);
+  marker.addListener('click', toggleBounce);
 }
 
-var marker;
 function toggleBounce() {
   if (marker.getAnimation() !== null) {
     marker.setAnimation(null);
@@ -36,6 +63,7 @@ Kinear Park
 Uncle Ike's
 Showbox Sodo
 Petite Toulouse
+Gameworks
 */
 
 
